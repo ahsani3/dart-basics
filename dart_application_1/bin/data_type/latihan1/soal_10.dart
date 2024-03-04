1be4dc8ceec0326229643a867d94923d339d8c80
ahsani3 <ahsaniweb@gmail.com>
2024-03-04 15:05:28 +0700

    mengerjakan latihan 1 soal nomor 10

---
+++ dart_application_1/bin/data_type/latihan1/soal_10.dart
@@ -0,0 +1,7 @@
+void main(List<String> args) {
+  // no 10
+  String angkaTeks = '123';
+  int konfersi = int.parse(angkaTeks);
+  print('${angkaTeks} bertipe ${angkaTeks.runtimeType}');
+  print('${konfersi} bertipe ${konfersi.runtimeType}');
+}
