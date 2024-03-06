b5e1420a1de58117ba6aac7fc88fa7accd558df9
ahsani3 <ahsaniweb@gmail.com>
2024-03-06 11:55:11 +0700

    mengerjakan latihan 2 soal nomor 10

---
+++ dart_application_1/bin/data_type/latihan2/soal_10.dart
@@ -0,0 +1,9 @@
+void main(List<String> args) {
+  // no 10
+  List kata = ['Muhammad', 'Ahsani', 'Nur', 'Taqwimi'];
+  print(kata);
+  for (String kat in kata) {
+    int panjkata = kat.length;
+    print('$kat $panjkata');
+  }
+}
